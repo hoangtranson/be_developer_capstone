729050970471f3c596ea60b24b45b21d9624aa37
hoangtranson <son.hoang01@gmail.com>
2023-03-12 22:13:19 +0700

    add sample api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import MenuView, BookingView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', MenuView.as_view()),
+    path('booking/', BookingView.as_view())
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,11 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 
-# Create your views here.
-def say_hello(request):
-    return HttpResponse('hello')
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
 
 
-def index(request):
-    return render(request, 'index.html', {})
+class MenuView(APIView):
+
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "status": "success",
+                "data": serializer.data
+            })
